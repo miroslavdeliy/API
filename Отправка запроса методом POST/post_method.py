34697f0600f9fe960d6329c8643e168c9e8d444c
Miroslav <miroslavdely@gmail.com>
2025-03-11 18:11:11 +0300

    2-st version

--- Отправка запроса методом POST/post_method.py
+++ Отправка запроса методом POST/post_method.py
@@ -2,17 +2,20 @@
 import requests
 
 # Создание общего класса
-class test_new_location():
-    # Метод для теста
-    def test_create_new_location(self):
-        base_url = 'https://rahulshettyacademy.com'
-        key = '?key=qaclick123'
-        post_resourse = '/maps/api/place/add/json'
-        get_resourse = '/maps/api/place/get/json'
-        place_id_list = []
+class test_new_location:
 
+    # Конструктор
+    def __init__(self):
+        self.base_url = 'https://rahulshettyacademy.com'
+        self.key = '?key=qaclick123'
+        self.post_resourse = '/maps/api/place/add/json'
+        self.get_resourse = '/maps/api/place/get/json'
+        self.place_id_list = []
+
+    # Метод запроса POST
+    def test_post_method(self, status, file_name):
         # Формирование запроса POST
-        post_url = base_url + post_resourse + key
+        post_url = self.base_url + self.post_resourse + self.key
         print(post_url)
         # Body
         json_location = {
@@ -34,25 +37,27 @@ class test_new_location():
         # Сохранение 5 place_id в список
         for _ in range(5):
             result_post = requests.post(post_url, json=json_location)
-            assert result_post.status_code == 200, 'Ошибка! Статус код неверный!'
+            assert result_post.status_code == status, 'Ошибка! Статус код неверный!'
             print('Статус код POST корректный')
             check_response_post = result_post.json()
             print(check_response_post)
-            place_id_list.append(check_response_post.get('place_id'))
+            self.place_id_list.append(check_response_post.get('place_id'))
 
         #Отправление списка в файл
         try:
-            with open('place_id.txt', 'w') as file:
-                file.writelines(f'{one_place_id} ' for one_place_id in place_id_list)
+            with open(file_name, 'w') as file:
+                file.writelines(f'{one_place_id} ' for one_place_id in self.place_id_list)
                 print('Список отправлен в файл!')
         except PermissionError:
             print('Нет прав доступа к файлу')
         except IOError:
             print('Ошибка записи в файл!')
 
+    # Метод запроса GET
+    def test_get_method(self, status, file_name):
         #Чтение из файла и формирование списка
         try:
-            with open ('place_id.txt', 'r') as file:
+            with open (file_name, 'r') as file:
                 place_id_file = file.read().split()
                 print('Список считали из файла')
         except FileNotFoundError:
@@ -64,15 +69,16 @@ class test_new_location():
 
         # Отправка запроса GET по всем place_id из списка
         for one_place_id in place_id_file:
-           get_url = base_url + get_resourse + key + '&place_id=' + one_place_id
+           get_url = self.base_url + self.get_resourse + self.key + '&place_id=' + one_place_id
            print(get_url)
            result_get = requests.get(get_url)
            print(result_get.json())
            print(f'Статус-код: {result_get.status_code}')
-           assert result_get.status_code == 200, 'Ошибка Статус код GET некорректен!'
+           assert result_get.status_code == status, 'Ошибка Статус код GET некорректен!'
            print('Статус код GET корректен')
 
 
 start = test_new_location()
-start.test_create_new_location()
+start.test_post_method(200, 'file_id.txt')
+start.test_get_method(200, 'file_id.txt')
 print('\nТест прошел успешно')
